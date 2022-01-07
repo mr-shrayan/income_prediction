7825c4f9db6b0b79094771ba031653de030cdc57
mr-shrayan <theemptyunexplored@gmail.com>
2022-01-07 19:47:34 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To predict the income of an individual after getting the input of different details about that individual such as age, workclass, education level, etc.',
+    author='Sagnik Chakraborty & Shrayan Bandyopadhyay',
+    license='MIT',
+)
